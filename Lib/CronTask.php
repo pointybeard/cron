e49ad4fb2e204d42865871c3ed6af4715fc4289e
Alistair Kearney <hi@alistairkearney.com>
2018-09-23 15:06:54 +1000

    Tasks are now saved as JSON instead of XML. Updated to extend pointybeard/property-bag and use pointybeard/symphony-pdo for database access. Added 'force' property and streamlined saving process.

--- Lib/CronTask.php
+++ Lib/CronTask.php
@@ -1,232 +1,262 @@
 <?php
 namespace Cron\Lib;
 
-final class CronTask
+use SymphonyPDO;
+use pointybeard\PropertyBag;
+
+class CronTask extends PropertyBag\Lib\PropertyBag
 {
-    private $db;
+    const FORCE_EXECUTE_YES = 'yes';
+    const FORCE_EXECUTE_NO = 'no';
 
-    private $_properties = [
-        'path' => NULL,
-        'filename' => NULL,
-        'name' => NULL,
-        'description' => NULL,
-        'interval' => NULL,
-        'interval-type' => 'minute',
-        'command' => NULL,
-        'enabled' => false,
-        'last_executed' => NULL,
-        'last_output' => NULL,
-        'start' => NULL,
-        'finish' => NULL,
-    ];
+    const DURATION_MINUTE = "minutes";
+    const DURATION_DAY = "day";
+    const DURATION_HOUR= "hour";
+    const DURATION_WEEK = "week";
 
-    public function load($path)
-    {
-        if (!file_exists($path)) {
-            throw new \Exception('Task could not be written to `'.$path.'`');
-        }
+    const ENABLED = 1;
+    const DISABLED = 0;
 
-        $this->path = $path;
-        $doc = new \SimpleXMLElement($this->path, null, true);
+    const SAVE_MODE_DATABASE_ONLY = 0;
+    const SAVE_MODE_FILE_ONLY = 1;
+    const SAVE_MODE_BOTH = 2;
 
-        $this->filename = basename($this->path);
-        $this->name = (string) $doc->name;
-        if (isset($doc->description) && strlen($doc->description) > 0) {
-            $this->description = (string) $doc->description;
-        }
-        if (isset($doc->start) && !is_null($doc->start)) {
-            $this->start = strtotime($doc->start);
-        }
-        if (isset($doc->finish) && !is_null($doc->finish)) {
-            $this->finish = strtotime($doc->finish);
-        }
-
-        $this->setInterval((int) $doc->interval, (isset($doc->interval->attributes()->type) ? (string) $doc->interval->attributes()->type : 'minute'));
-
-        $this->command = (string) $doc->command;
-
-        $this->enabled = false;
-
-        $this->last_executed = $this->last_output = null;
-        $this->enabled = false;
-
-        $row = $this->db->fetchRow(0, sprintf(
-            "SELECT * FROM `tbl_cron` WHERE `name` = '%s' LIMIT 1",
-            $this->filename
-        ));
-
-        if (is_array($row) && !empty($row)) {
-            $this->last_executed = (!is_null($row['last_executed']) ? strtotime($row['last_executed']) : null);
-            $this->last_output = $row['last_output'];
-            $this->enabled = (bool) $row['enabled'];
-        }
-
-        return $this;
+    public function __construct() {
+        // IMPORTANT: Must initialise the entire propertybag object
+        // otherwise using the __get and __set methods in this class
+        // with fail.
+        $this
+            ->path(NULL)
+            ->filename(NULL)
+            ->name(NULL)
+            ->description(NULL)
+            ->interval(
+                (new PropertyBag\Lib\PropertyBag)
+                    ->type(self::DURATION_MINUTE)
+                    ->duration(NULL)
+            )
+            ->command(NULL)
+            ->enabled(self::DISABLED)
+            ->force(self::FORCE_EXECUTE_NO)
+            ->lastExecuted(NULL)
+            ->lastOutput(NULL)
+            ->start(NULL)
+            ->finish(NULL)
+        ;
     }
 
-    public function __construct($db)
-    {
-        $this->db = $db;
+    public static function load($path) {
+
+        $path = realpath($path);
+
+        if (!file_exists($path) || !is_readable($path)) {
+            throw new Exceptions\LoadingTaskFailedException($path, 'File does not exist or is not readable.');
+        }
+
+        $data = json_decode(file_get_contents($path));
+        if(is_null($data)) {
+            throw new Exceptions\LoadingTaskFailedException($path, 'Task is invalid JSON and cannot be read.');
+        }
+
+        $task = (new self)
+            ->path($path)
+            ->filename(basename($path))
+            ->name($data->name)
+            ->interval(
+                (new PropertyBag\Lib\PropertyBag)
+                    ->type($data->interval->type)
+                    ->duration($data->interval->duration)
+            )
+            ->command($data->command)
+        ;
+
+        if(isset($data->description)) {
+            $task->description($data->description);
+        }
+
+        if(isset($data->start)) {
+            $task->start($data->start);
+        }
+
+        if(isset($data->finish)) {
+            $task->finish($data->finish);
+        }
+
+        $query = SymphonyPDO\Loader::instance()->prepare(
+            'SELECT * FROM `tbl_cron` WHERE `name` = :name LIMIT 1'
+        );
+
+        $taskName = (string)$task->filename; //PDO complains if we do this directly below
+        $query->bindParam(':name', $taskName, \PDO::PARAM_STR);
+
+        $query->execute();
+        $result = $query->fetchObject();
+
+        if($result !== false) {
+            $task
+                ->lastExecuted($result->last_executed)
+                ->lastOutput($result->last_output)
+                ->force($result->force_execution)
+                ->enabled((string)$result->enabled == 'yes' ? self::ENABLED : self::DISABLED)
+            ;
+        }
+
+        return $task;
     }
 
-    public function getLog()
-    {
-        return $this->last_output;
+    public function getLog() {
+        return $this->lastOutput->value;
     }
 
-    public function getLastExecutionTimestamp()
-    {
-        return $this->last_executed;
+    public function getLastExecutionTimestamp() {
+        return $this->lastExecuted->value;
     }
 
-    public function run()
-    {
+    public function run() {
         if ($this->enabledReal() !== true || $this->nextExecution() != 0) {
             return;
         }
 
-        $this->last_output = shell_exec($this->command);
-        $this->last_executed = time();
-
-        $sql = sprintf(
-                "UPDATE `tbl_cron` SET `last_output` = '%s', `last_executed` = '%s' WHERE `name` = '%s' LIMIT 1",
-                $this->db->cleanValue($this->last_output),
-                \DateTimeObj::get('Y-m-d H:i:s', $this->last_executed),
-                $this->filename
-            );
-
-        $this->db->query($sql);
+        $this
+            ->lastOutput(shell_exec((string)$this->command))
+            ->lastExecuted(time())
+            ->save(self::SAVE_MODE_DATABASE_ONLY)
+        ;
     }
 
-    public function enabledReal()
-    {
-        return ($this->enabled == true && !is_null($this->nextExecution()));
+    public function enabledReal() {
+        return (
+            $this->enabled->value == self::ENABLED &&
+            !is_null($this->nextExecution())
+        );
     }
 
-    public function intervalReal()
-    {
-        $value = $this->interval;
+    public function intervalReal() {
+        $value = (int)$this->interval->value->duration;
 
-        switch ($this->{'interval-type'}) {
+        switch ((string)$this->interval->value->type) {
 
-                case 'week':
-                    $value *= 7;
+            case self::DURATION_WEEK:
+                $value *= 7;
 
-                case 'day':
-                    $value *= 24;
+            case self::DURATION_DAY:
+                $value *= 24;
 
-                case 'hour':
-                    $value *= 60;
-                    break;
+            case self::DURATION_HOUR:
+                $value *= 60;
+                break;
 
-            }
+        }
 
         return $value;
     }
 
-    public function setInterval($value, $type = 'minute')
-    {
-        $this->{'interval-type'} = $type;
-        $this->interval = $value;
+    public function setInterval($value, $type = self::DURATION_MINUTE) {
+        $this->interval->value->type = $type;
+        $this->interval->value->duration = $value;
+        return $this;
     }
 
-    public function nextExecution()
-    {
-        $next_execution = null;
+    public function nextExecution() {
+        $nextExecution = null;
 
-        if ($this->enabled == true) {
-            if (!is_null($this->finish) && time() >= $this->finish) {
-                $next_execution = null;
-            } elseif (!is_null($this->start) && $this->start > time()) {
-                $next_execution = $this->start - time();
-            } elseif (is_null($this->last_executed)) {
-                $next_execution = 0;
+        if ($this->enabled->value == self::ENABLED) {
+            if($this->force->value == self::FORCE_EXECUTE_YES) {
+                $nextExecution = 0;
+
+            } elseif (!is_null($this->finish->value) && time() >= $this->finish->value) {
+                $nextExecution = null;
+
+            } elseif (!is_null($this->start->value) && $this->start->value > time()) {
+                $nextExecution = $this->start->value - time();
+
+            } elseif (is_null($this->lastExecuted->value)) {
+                $nextExecution = 0;
+
             } else {
-                $next_execution = max(0, ($this->last_executed + ($this->intervalReal() * 60)) - time());
+                $nextExecution = max(0, ($this->lastExecuted->value + ($this->intervalReal() * 60)) - time());
             }
         }
 
-        return $next_execution;
+        return $nextExecution;
     }
 
-    public function __toString()
-    {
-        $doc = new \DOMDocument('1.0', 'UTF-8');
-        $doc->formatOutput = true;
-        $doc->preserveWhitespace = false;
+    public function __toString() {
+        return json_encode($this->toArray(), JSON_PRETTY_PRINT);
+    }
 
-        $root = $doc->createElement('task');
-        $doc->appendChild($root);
+    public function delete() {
+        if (!\General::deleteFile((string)$this->path)) {
+            throw new \Exception('Task `'.(string)$this->path.'` could not be deleted');
+        }
+        SymphonyPDO\Loader::instance()->delete(
+            'tbl_cron', sprintf("`name` = '%s' LIMIT 1", $this->filename)
+        );
+    }
 
-        $root->appendChild($doc->createElement('command', $this->command));
-        $root->appendChild($doc->createElement('name', $this->name));
+    public function save($saveMode=self::SAVE_MODE_BOTH, \Closure $writeFunction = NULL) {
 
-        if (!is_null($this->description)) {
-            $root->appendChild($doc->createElement('description', $this->description));
+        // Create a default write function in case one is not provided
+        if(is_null($writeFunction)) {
+            $writeFunction = function ($file, $data) {
+                return @file_put_contents($file, $data);
+            };
         }
 
-        if (!is_null($this->start)) {
-            $root->appendChild($doc->createElement('start', \DateTimeObj::get('Y-m-d H:i:s', $this->start)));
-        }
-        if (!is_null($this->finish)) {
-            $root->appendChild($doc->createElement('finish', \DateTimeObj::get('Y-m-d H:i:s', $this->finish)));
-        }
+        $data = $this->toArray();
 
-        $interval = $doc->createElement('interval', $this->interval);
-        $interval->setAttribute('type', $this->{'interval-type'});
-        $root->appendChild($interval);
+        $query = SymphonyPDO\Loader::instance()->prepare(
+            "INSERT INTO `tbl_cron`
+            (
+                `name`,
+                `last_executed`,
+                `enabled`,
+                `last_output`,
+                `force_execution`
 
-        return $doc->saveXML();
-    }
+            ) VALUES (
+                :filename,
+                :lastExecuted,
+                :enabled,
+                :lastOutput,
+                :forceExecution
+            )
 
-    public function __set($name, $value)
-    {
-        $this->_properties[$name] = $value;
-    }
+            ON DUPLICATE KEY UPDATE
+                `last_executed` = :lastExecuted,
+                `last_output` = :lastOutput,
+                `enabled` = :enabled,
+                `force_execution` = :forceExecution"
+        );
 
-    public function __get($name)
-    {
-        return $this->_properties[$name];
-    }
+        $enabled = ($data['enabled'] == self::ENABLED ? 'yes' : 'no');
 
-    public function enable()
-    {
-        return $this->db->query(sprintf(
-            "INSERT INTO `tbl_cron` VALUES ('%s', NULL, 1, NULL) ON DUPLICATE KEY UPDATE `enabled` = 1",
-            $this->db->cleanValue($this->filename)
-        ));
-    }
+        $query->bindParam(':filename', $data['filename'], \PDO::PARAM_STR);
+        $query->bindParam(':lastExecuted', $data['lastExecuted'], \PDO::PARAM_STR);
+        $query->bindParam(':lastOutput', $data['lastOutput'], \PDO::PARAM_STR);
+        $query->bindParam(':enabled', $enabled, \PDO::PARAM_STR);
+        $query->bindParam(':forceExecution', $data['force'], \PDO::PARAM_STR);
 
-    public function disable()
-    {
-        return $this->db->query(sprintf(
-            "INSERT INTO `tbl_cron` VALUES ('%s', NULL, 0, NULL) ON DUPLICATE KEY UPDATE `enabled` = 0",
-            $this->db->cleanValue($this->filename)
-        ));
-    }
+        // This data should not be going into the file since it is
+        // specific to this installation
+        unset($data['path']);
+        unset($data['filename']);
+        unset($data['lastExecuted']);
+        unset($data['lastOutput']);
+        unset($data['enabled']);
+        unset($data['force']);
 
-    public function delete()
-    {
-        if (!\General::deleteFile($this->path)) {
-            throw new \Exception('Task `'.$this->path.'` could not be deleted');
-        }
-
-        return $this->db->query(sprintf(
-            "DELETE FROM `tbl_cron` WHERE `name` = '%s' LIMIT 1",
-            $this->db->cleanValue($this->filename)
-        ));
-    }
-
-    public function save(\Closure $fileWriteFunc)
-    {
-        if (!$fileWriteFunc($this->path, (string) $this)) {
-            throw new \Exception('Task could not be written to `'.$this->path.'`');
+        if($saveMode != self::SAVE_MODE_DATABASE_ONLY) {
+            if ($writeFunction($this->path, json_encode($data, JSON_PRETTY_PRINT))  === false) {
+                throw new Exceptions\WritingTaskFailedException($this->path);
+            }
         }
 
-        return $this->db->query(sprintf(
-            "INSERT INTO `tbl_cron` VALUES ('%s', NULL, %d, NULL) ON DUPLICATE KEY UPDATE `enabled` = %2\$d",
-            $this->db->cleanValue($this->filename),
-            (int) $this->enabled
-        ));
+        // File written. Now update the database
+        if($saveMode != self::SAVE_MODE_FILE_ONLY) {
+            $result = $query->execute();
+        }
+
+        return true;
     }
 }
