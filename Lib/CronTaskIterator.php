98f43e7072800af6a41e9ffc91ef3fc93a280928
Alistair Kearney <hi@alistairkearney.com>
2018-09-23 15:02:37 +1000

    No longer required to pass in a connection to the database. Using pointybeard/symphony-pdo now instead.

--- Lib/CronTaskIterator.php
+++ Lib/CronTaskIterator.php
@@ -3,63 +3,61 @@ namespace Cron\Lib;
 
 final class CronTaskIterator implements \Iterator, \Countable
 {
-    private $_iterator = null;
+    private $iterator = null;
     private $count = null;
-    private $db = null;
 
-    public function __construct($directory, $db)
+    public function __construct($directory)
     {
-        $this->db = $db;
-        $this->_iterator = new CronTaskFilterIterator($directory);
+        $this->iterator = new CronTaskFilterIterator($directory);
     }
 
     public function current()
     {
-        $this->_current = $this->_iterator->current();
-
-        return (new CronTask($this->db))->load($this->_current->getPathname());
+        return CronTask::load(
+            $this->iterator->current()->getPathname()
+        );
     }
 
     public function innerIterator()
     {
-        return $this->_iterator;
+        return $this->iterator;
     }
 
     public function next()
     {
-        $this->_iterator->next();
+        $this->iterator->next();
     }
 
     public function key()
     {
-        return $this->_iterator->key();
+        return $this->iterator->key();
     }
 
     public function valid()
     {
-        return $this->_iterator->valid();
+        return $this->iterator->valid();
     }
 
     public function rewind()
     {
-        $this->_iterator->rewind();
+        $this->iterator->rewind();
     }
 
     public function position()
     {
-        throw new Exception('CronTaskIterator::position() cannot be called.');
+        throw new \Exception('CronTaskIterator::position() cannot be called.');
     }
 
     public function length()
     {
-        throw new Exception('CronTaskIterator::length() cannot be called.');
+        throw new \Exception('CronTaskIterator::length() cannot be called.');
     }
 
     public function count()
     {
-        if (is_null($this->count) && $this->_iterator instanceof CronTaskFilterIterator) {
-            $this->count = iterator_count($this->_iterator);
-            $this->_iterator->rewind();
+        if (is_null($this->count) && $this->iterator instanceof CronTaskFilterIterator) {
+            $this->count = iterator_count($this->iterator);
+            $this->iterator->rewind();
         }
 
         return $this->count;
